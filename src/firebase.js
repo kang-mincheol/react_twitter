a84d515dc99e32144a4b009d52d433cacc7dc117
mincheol <aaa6315@gmail.com>
2023-04-17 19:42:10 +0900

    firebase config 설정

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjA3CS9SfIbf_-b3TgkefhRCJq3rV8MzM",
+  authDomain: "react-twitter-fad39.firebaseapp.com",
+  projectId: "react-twitter-fad39",
+  storageBucket: "react-twitter-fad39.appspot.com",
+  messagingSenderId: "111178491486",
+  appId: "1:111178491486:web:8d177454fafb114451b6cf"
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
